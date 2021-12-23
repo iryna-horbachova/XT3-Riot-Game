790475e4c2c287ea14da50e2e4a22c40cf12a924
iryna-horbachova <irachka.gorbacheva@gmail.com>
2021-12-23 17:16:00 +0200

    Add flag indication of level finish

---
+++ Assets/Scripts/LevelManagement/Finish.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private AudioSource finishSoundEffect;
+
+    private bool levelCompleted = false;
+
+    void Start()
+    {
+        finishSoundEffect = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "OrangeRobot" && !levelCompleted)
+        {
+            finishSoundEffect.Play();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
+        }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
